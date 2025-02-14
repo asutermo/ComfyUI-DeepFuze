cfd93274245185b87a139e6a19aa3c08583edecc
andrew <andrew@forming.ai>
2025-02-14 13:37:25 -0700

    remove tts pycache

--- tts_generation.py
+++ tts_generation.py
@@ -11,16 +11,20 @@ from TTS.api import TTS
 
 def main():
     parser = argparse.ArgumentParser(description="Run TTS with specified parameters.")
-    
+
     parser.add_argument('--model', type=str, default="tts_models/multilingual/multi-dataset/xtts_v2", help="The TTS model to use.")
     parser.add_argument('--text', type=str, required=True, help="The text to be converted to speech.")
     parser.add_argument('--speaker_wav', type=str, required=True, help="The path to the speaker's wav file for voice cloning.")
     parser.add_argument('--language', type=str, default="en", help="The language of the text.")
     parser.add_argument('--output_file', type=str, default="output.wav", help="The output file path for the synthesized speech.")
     parser.add_argument('--device', type=str, choices=["cpu", "mps","cuda"], default="cpu" if torch.cuda.is_available() else "cpu", help="The device to run the model on.")
-    
+
     args = parser.parse_args()
-	
+
+    if not os.path.exists(args.model):
+        print(f"Model path {args.model} does not exist.")
+        return 1
+
     if args.device == "cuda":
         device="cuda"
     else:
